987336971125784f86dcd682b4d52221c25bbabd
Sannikov Piotr <petrovichqmail@gmail.com>
2019-09-12 11:15:52 +0300

    Start new project

---
+++ src/main/java/com/home/study/OptionalTask1.java
@@ -0,0 +1,11 @@
+package com.home.study;
+
+import java.util.Scanner;
+
+public class OptionalTask1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        
+    }
+}
